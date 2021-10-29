ea3593c5317a5a61782a4e6cbe7be2341985ecc0
phohwell2 <phowell2@stevens.edu>
2021-10-29 14:35:49 -0400

    5

---
+++ cmake-build-debug/CMakeFiles/Lab_8_CPE390.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab_8_CPE390.dir/GravSim.cc.o"
+  "CMakeFiles/Lab_8_CPE390.dir/main.cc.o"
+  "Lab_8_CPE390"
+  "Lab_8_CPE390.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab_8_CPE390.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
